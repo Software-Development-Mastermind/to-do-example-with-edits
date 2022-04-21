eae07653908a44dacb5a888c21388bc3337063a7
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-04-21 18:57:00 +0800

    initial

---
+++ scripts.js
@@ -0,0 +1,33 @@
+function toDo(entry){
+  document.write = window.prompt()
+}
+
+toDo("Go to the gym");
+
+var enter = onclick;
+
+var add = document.getElementById("Add");
+
+
+
+function init() {
+  var addButton = document.getElementById("Add");
+  addButton.onclick = handleAdd;
+}
+
+function handleAdd() {
+  var newItem = ;
+}
+
+function handleKeyPress(e) {
+  var addButton = document.getElementById("Add");
+  if (e.keyCode === 13) {
+    addButton.click();
+    return false;
+  }
+}
+
+function flexBox(msg) {
+var entry = document.getElementById("entry");
+msg = 
+}
