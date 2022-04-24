c8891af99ab416be53a8939d61b1e1dca8b6fb7f
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-04-24 18:24:50 +0800

    next

--- scripts.js
+++ scripts.js
@@ -1,33 +1,20 @@
-function toDo(entry){
-  document.write = window.prompt()
-}
-
-toDo("Go to the gym");
-
-var enter = onclick;
-
-var add = document.getElementById("Add");
-
-
+window.onload = init;
 
 function init() {
   var addButton = document.getElementById("Add");
   addButton.onclick = handleAdd;
 }
 
-function handleAdd() {
-  var newItem = ;
+ function handleAdd(){
+  
+  var node = document.createElement("Li");
+
+  var text = document.getElementById("entry").value;
+
+  var textnode = document.createTextNode(text);
+
+  node.appendChild(textnode);
+
+  document.getElementById("list").appendChild(node);
 }
 
-function handleKeyPress(e) {
-  var addButton = document.getElementById("Add");
-  if (e.keyCode === 13) {
-    addButton.click();
-    return false;
-  }
-}
-
-function flexBox(msg) {
-var entry = document.getElementById("entry");
-msg = 
-}
