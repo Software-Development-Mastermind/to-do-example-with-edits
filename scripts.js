77a60344beceb5b53315f5d0efcd2960535fd637
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-04-29 14:26:32 +0800

    most recent

--- scripts.js
+++ scripts.js
@@ -7,6 +7,9 @@ function init() {
   
   const addItem = document.getElementById("entry");
       addItem.onkeydown = handleKeyPress;
+  
+  const removeItem = document.getElementById("Li"); 
+        removeItem.onclick = handleRemove;
 }
 
 function handleKeyPress(e) {
@@ -18,23 +21,35 @@ function handleKeyPress(e) {
 }
 
  function handleAdd(){
-  
+
   const node = document.createElement("Li");
 
   const text = document.getElementById("entry").value;
-      node.textContent = text;
+      node.textContent = text;  
 
   const checkBox = document.createElement("input");
       checkBox.setAttribute("type", "checkbox"); 
       node.appendChild(checkBox)
       checkBox.style.cssFloat = "left";
   
-  
-  //    const delete = document.createElement("input");
-
-  //    delete.setAttribute("type", "button"); 
-  //   node.appendChild(delete);
-  //    checkBox.style.cssFloat = "right";
+    //checkBox.checked.className = "strikethrough";
+   
 
   document.getElementById("list").appendChild(node); 
+
+  return checkBox;
 }
+
+
+
+function handleRemove(checkBox) {
+  if (checkBox.checked) {
+    document.getElementById("Li").style.textDecoration = "line-through";
+  }
+  else {
+    document.getElementById("Li").style.textDecoration = "none";
+  }
+ }
+
+
+  
