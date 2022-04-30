b81a341b04288498e44733873fafb101d13a5fa2
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-04-30 18:02:47 +0800

    progress

--- scripts.js
+++ scripts.js
@@ -1,55 +1,57 @@
+
 window.onload = init;
 
 
 function init() {
-  const addButton = document.getElementById("Add");
-      addButton.onclick = handleAdd;
+    const addButton = document.getElementById("Add");
+        addButton.onclick = handleAdd;
   
-  const addItem = document.getElementById("entry");
-      addItem.onkeydown = handleKeyPress;
-  
-  const removeItem = document.getElementById("Li"); 
-        removeItem.onclick = handleRemove;
+    const addItem = document.getElementById("entry");
+        addItem.onkeydown = handleKeyPress;
 }
 
+
 function handleKeyPress(e) {
-  const addButton = document.getElementById("Add");
-  if (e.keyCode === 13) {
-    addButton.click();
-    return false;
+    const addButton = document.getElementById("Add");
+        if (e.keyCode === 13) {
+        addButton.click();
+        return false;
   }
 }
+
 
  function handleAdd(){
 
-  const node = document.createElement("Li");
+    const node = document.createElement("Li");
 
-  const text = document.getElementById("entry").value;
-      node.textContent = text;  
+    const text = document.getElementById("entry").value;
+        node.textContent = text;  
 
-  const checkBox = document.createElement("input");
-      checkBox.setAttribute("type", "checkbox"); 
-      node.appendChild(checkBox)
-      checkBox.style.cssFloat = "left";
-  
-    //checkBox.checked.className = "strikethrough";
-   
+    const checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox"); 
+        node.appendChild(checkBox)
+        checkBox.style.cssFloat = "left";
+
+        checkBox.addEventListener("click", () => 
+            addLineThrough(node));
+
+    const button = document.createElement("input");
+        button.setAttribute("type","button", "class=button2");
+        node.appendChild(button)
+        button.style.cssFloat = "right";
+
+        button.addEventListener("click", () => 
+        handleRemove(text));
+
 
   document.getElementById("list").appendChild(node); 
-
-  return checkBox;
 }
 
 
-
-function handleRemove(checkBox) {
-  if (checkBox.checked) {
-    document.getElementById("Li").style.textDecoration = "line-through";
-  }
-  else {
-    document.getElementById("Li").style.textDecoration = "none";
-  }
+ function addLineThrough(element) {
+        element.classList.toggle("myLine");
  }
 
-
-  
+ function handleRemove(item) {
+        item.classList.removeChild("entry");
+ }
